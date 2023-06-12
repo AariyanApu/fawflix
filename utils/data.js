1b2f958c2602e09f40a73e4548d582a9c58549c8
Aariyan Apu <38152118+AariyanApu@users.noreply.github.com>
2023-06-12 07:08:14 +0600

    add data

---
+++ utils/data.js
@@ -0,0 +1,22 @@
+export const slideImages = [
+  {
+    imageLink: '/assets/images/banner1.jpg',
+    movieLink: '/',
+  },
+  {
+    imageLink: '/assets/images/banner2.jpg',
+    movieLink: '/',
+  },
+  {
+    imageLink: '/assets/images/banner3.jpg',
+    movieLink: '/',
+  },
+  {
+    imageLink: '/assets/images/banner2.jpg',
+    movieLink: '/',
+  },
+  {
+    imageLink: '/assets/images/banner4.jpg',
+    movieLink: '/',
+  },
+];
