3bae1d655c7a04eab913a5d814e40f646b44b48a
Aariyan Apu <38152118+AariyanApu@users.noreply.github.com>
2023-06-12 07:07:58 +0600

    added json data to slide image

--- src/component/Feed.jsx
+++ src/component/Feed.jsx
@@ -5,21 +5,40 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
+import Image from 'next/image';
+import Link from 'next/link';
+import { slideImages } from '../../utils/data';
+
 const Feed = () => {
   return (
     <Swiper
       pagination={{
         dynamicBullets: true,
+        clickable: true,
       }}
       modules={[Pagination]}
+      loop={true}
+      grabCursor={true}
+      loopFillGroupWithBlank={true}
+      autoplay={{
+        delay: 2000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+      }}
       className="mySwiper"
     >
-      <SwiperSlide>
-        <img src="/assets/images/banner1.jpg" className="h-12" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src="/assets/images/banner2.jpg" className="h-12" />
-      </SwiperSlide>
+      {slideImages.map((slide, index) => (
+        <SwiperSlide key={index}>
+          <Link href={slide.movieLink}>
+            <Image
+              src={slide.imageLink}
+              alt="Slide-image"
+              width={1400}
+              height={500}
+            />
+          </Link>
+        </SwiperSlide>
+      ))}
     </Swiper>
   );
 };
