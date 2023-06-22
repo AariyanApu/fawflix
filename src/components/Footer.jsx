4ef00f66dbfe20d603ba267dbaea5fe702cb3144
Aariyan Apu <aariyan.apu@gmail.com>
2023-06-23 03:55:26 +0600

    make footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,18 +9,18 @@ export default function Footer() {
           <div>
             {/* Request to Upload Movie  */}
 
-            <h3 className="text-2xl font-semibold leading-6 red_gradient">
+            <h3 className="sm:text-2xl text-lx font-semibold leading-6 red_gradient text-center sm:text-left">
               {' '}
               The Movie... You wish to watch !{' '}
             </h3>
-            <p className="mt-2 text-sm leading-6 red_gradient">
+            <p className="mt-2 text-xs leading-6 red_gradient">
               Please write the name of that movie, and we will upload that.
             </p>
           </div>
 
           {/* Request Form  */}
 
-          <form className="mt-6 flex sm:max-w-md">
+          <form className="mt-6 mb-5 sm:mb-0 flex sm:max-w-md flex-col sm:flex-row">
             <label htmlFor="email-address" className="sr-only">
               Write the movie Name
             </label>
@@ -69,7 +69,7 @@ export default function Footer() {
       {/* Disclaimer  */}
 
       <div className="mt-8 border-t border-red-600/60 pt-8 md:flex md:items-center md:justify-between text-center sm:text-right">
-        <p className="text-xs leading-5 red_gradient sm:w-[400px] w-80 mx-auto sm:mx-0">
+        <p className="text-xs leading-5 red_gradient sm:w-[400px] w-80 mx-auto sm:mx-0 pb-2 sm:pb-0">
           This website is for educational purpose only. We do not host any files
           on our server. All contents are provided by non-affiliated third
           parties.
