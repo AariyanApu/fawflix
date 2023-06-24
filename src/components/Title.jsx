ac5ce8c0a9875f159a9349224e4b6a5a92f9af4f
Aariyan Apu <aariyan.apu@gmail.com>
2023-06-24 15:16:21 +0600

    fix title bug

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Title({ title, link, linkName }) {
   return (
-    <div className="flex flex-row justify-between items-center mb-2 mx-2 sm:mx-0">
+    <div className="flex flex-row justify-between items-center mb-2 px-10 sm:px-0">
       <p className="orange_gradient text-2xl">{title}</p>
       <div className="flex">
         <div className=" mt-3 h-0 w-40 border-red-600 border  md:w-[500px] lg:w-[800px] xl:w-[900] md:flex hidden " />
