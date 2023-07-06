eb0cb8d3930e59515a301e581a48733621a97386
Aariyan Apu <38152118+AariyanApu@users.noreply.github.com>
2023-07-07 02:08:40 +0600

    added instant delete

--- src/utils/GetDataApi.js
+++ src/utils/GetDataApi.js
@@ -2,11 +2,12 @@ import useSWR from 'swr';
 
 export function useUser() {
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
-  const { data, error, isLoading } = useSWR(`/api/posts/`, fetcher);
+  const { data, error, isLoading, mutate } = useSWR(`/api/posts/`, fetcher);
 
   return {
     data: data,
     isLoading,
     isError: error,
+    mutate,
   };
 }
