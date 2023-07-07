f1ad6458ff0aa452088941911e0f14c8d937902d
Aariyan Apu <38152118+AariyanApu@users.noreply.github.com>
2023-07-08 03:50:03 +0600

    fix search bug

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,18 +6,23 @@ import { useState } from 'react';
 export default function SearchBar() {
   const router = useRouter();
 
-  const [searchText, setSearchText] = useState('');
-  const handleSearchChange = (e) => {
-    setSearchText(e.target.value);
-    router.push(`/search/${searchText}`);
+  const [searchText, setSearchText] = useState({ title: ' ' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    router.push(`/search/${searchText.title}`);
+    setSearchText({ title: '' });
   };
+
   return (
-    <form className="">
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Search for a Movie or TV show..."
-        value={searchText}
-        onChange={handleSearchChange}
+        value={searchText.title}
+        onChange={(e) =>
+          setSearchText({ ...searchText, title: e.target.value })
+        }
         required
         className="sm:w-96 sm:h-12 w-36 h-8 mt-6 rounded-md border text-red-600 border-red-600 placeholder:text-red-600 bg-gray-900 py-2.5 pl-1 pr-2.5 sm:pl-5 sm:pr-12 text-sm shadow-lg font-medium focus:border-red focus:outline-none focus:ring-red-600 peer"
       />
