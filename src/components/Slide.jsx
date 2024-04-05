0a558bbc6f92a818b452ea3be8071c136f0bd0de
Aariyan Apu <aariyan.apu@gmail.com>
2024-04-05 23:14:37 +0600

    fix skelletion padding problem

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,22 +1,22 @@
-'use client';
+"use client";
 
-import { Autoplay, Pagination } from 'swiper';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import useSWR from 'swr';
+import { Autoplay, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
+import useSWR from "swr";
 
-import Link from 'next/link';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
+import Link from "next/link";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const Slide = () => {
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
-  const { data, isLoading, isError } = useSWR('/api/banner', fetcher);
+  const { data, isLoading, isError } = useSWR("/api/banner", fetcher);
 
   if (isLoading)
     return (
       <div>
-        <div className="sm:w-[1280px] sm:h-[500px] w-[340px] h-40 sm:my-2 break-inside-avoid rounded-lg border p-2 border-red-600 bg-gray-900/75 bg-clip-padding  backdrop-blur-xl backdrop-filter transition-all duration-500 animate-pulse overflow-hidden" />
+        <div className="mx-auto sm:w-[1280px] sm:h-[500px] w-[340px] h-40 sm:my-2 break-inside-avoid rounded-lg border p-2 border-red-600 bg-gray-900/75 bg-clip-padding  backdrop-blur-xl backdrop-filter transition-all duration-500 animate-pulse overflow-hidden" />
       </div>
     );
 
