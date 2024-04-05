39ec99d88465e3fb0f3edcab99387eadfa76b5fb
Aariyan Apu <aariyan.apu@gmail.com>
2024-04-05 18:39:22 +0600

    Added Og image icon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,14 +1,17 @@
-import Footer from '@/components/Footer';
-import NavBar from '@/components/NavBar';
-import Provider from '@/components/Provider';
-import localFont from 'next/font/local';
-import './globals.css';
+import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
+import Provider from "@/components/Provider";
+import localFont from "next/font/local";
+import "./globals.css";
 
-const primaryFont = localFont({ src: '../utils/roboto.ttf' });
+const primaryFont = localFont({ src: "../utils/roboto.ttf" });
 
 export const metadata = {
-  title: 'Faw Flix',
-  description: 'Stream your favorite movies and TV shows for free!',
+  title: "Faw Flix",
+  description: "Stream your favorite movies and TV shows for free!",
+  metadataBase: new URL("https://fawflix.vercel.app"),
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
 };
 
 export default function RootLayout({ children }) {
